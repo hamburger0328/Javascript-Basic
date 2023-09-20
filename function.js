2eaab5bf255abd82f85394f07c234dbfbd47e568
hamburger0328 <seohun8340@gmail.com>
2023-09-20 20:59:03 +0900

    update : 함수 2번에서 막힘,,,

--- function.js
+++ function.js
@@ -24,29 +24,44 @@ greet("할리");
 */
 
 function meetAt(year, month, day) {
+  let years = year <= 2023;
+  let months = month <= 12;
+  let days = day <= 31;
+  console.log(years);
+  console.log(months);
+  console.log(days);
 
-  if () {
+  if (years == true) {
     alert(year + "년");
-  } else if () {
-    alert(year + "년" + month + "월");
-  } else if () {
+  } else if (months == true) {
+    alert(`${year}년${month}월`);
+  } else if (days == true) {
     alert(`${year}/${month}/${day}`);
   }
 }
-meetAt();
+meetAt(2023, 12, 19);
 
 /*문제3.
-- findSmallestElement 함수를 구현해 주세요. findSmallestElement 의 arr 인자는 숫자 값으로만 이루어진 배열입니다. arr 의 값들 중 가장 작은 값을 리턴 해주세요. 만일 arr 가 비어있으면 0을 리턴 해주세요. 예를 들어, 다음과 같은 배열이 인자(input)으로 들어왔다면 0이 리턴 되어야 합니다.
+- findSmallestElement 함수를 구현해 주세요. 
+findSmallestElement 의 arr 인자는 숫자 값으로만 이루어진 배열입니다. 
+arr 의 값들 중 가장 작은 값을 리턴 해주세요. 
+만일 arr 가 비어있으면 0을 리턴 해주세요. 
+
+예를 들어, 다음과 같은 배열이 인자(input)으로 들어왔다면 0이 리턴 되어야 합니다.
 
 이용되는 배열
 [100,200,3,0,2,1]
 */
-function findSmallestElement(year, month, day) {}
+function findSmallestElement(num) {
+  let arr = [100, 200, 3, 0, 2, 1];
+  if (arr) {
+  }
+}
 findSmallestElement();
 
 /*문제4.
 - 돈을 매개변수로 받으면 몇개의 지폐와 동전이 필요한지 최소 개수를 계산해주는 함수를 만드시오
 예) 12300인 경우
 */
-function money() {}
+function money(won) {}
 money();
